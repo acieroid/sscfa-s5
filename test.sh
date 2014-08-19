f9d75cdbf7e6883f349ce9e0778aed173f8b8b12
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-08-19 16:11:52 +0200

    Add script to launch tests

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+function run_test {
+    TEST="$1"
+    RES=$(./main.byte "$TEST" | sed -En 's/Evaluation done: Val\((.*)\)/\1/p')
+    EXPECTED=$(sed -En 's|// Expected: (.*)$|\1|p' "$TEST")
+    if [ "$RES" != "$EXPECTED" ]; then
+        echo "Test failed: $TEST. Got $RES instead of $EXPECTED"
+    fi
+}
+
+for TEST in $(ls tests/*.s5); do
+    run_test $TEST
+done
